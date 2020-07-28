e545ed58bd89ef8adf0e9839fcff84f941cc06d0
Olaide <olaidemmanuel1@gmail.com>
2020-07-28 14:19:28 +0100

    Implementation of react form and useState hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from '../src/components/layout/Navbar';
 import Landing from '../src/components/layout/Landing';
-import Login from "../src/components/auth/Login";
-import Register from "../src/components/auth/Register";
+import Login from '../src/components/auth/Login';
+import Register from '../src/components/auth/Register';
 import './App.css';
 
 const App = () => {
@@ -11,9 +11,13 @@ const App = () => {
     <Router>
       <Fragment>
         <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
