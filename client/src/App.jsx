09ce9c474b6fe4ceb1ca2d6bcd01038ea6b8ccf5
delaquash <olaidemmanuel1@gmail.com>
2020-09-28 15:57:09 +0100

    alert component and action call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,45 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from '../src/components/layout/Navbar';
-import Landing from '../src/components/layout/Landing';
-import Login from '../src/components/auth/Login';
-import Register from '../src/components/auth/Register';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import './App.css';
 // Redux packages
 import { Provider } from 'react-redux';
 import store from './store';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Switch>
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/register' component={Register} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
+    return ( <
+        Provider store = { store } >
+        <
+        Router >
+        <
+        Fragment >
+        <
+        Navbar / >
+        <
+        Route exact path = '/'
+        component = { Landing }
+        /> <
+				section className = 'container' >
+				<Alert />
+        <
+        Switch >
+        <
+        Route exact path = '/login'
+        component = { Login }
+        /> <
+        Route exact path = '/register'
+        component = { Register }
+        /> < /
+        Switch > <
+        /section> < /
+        Fragment > <
+        /Router> < /
+        Provider >
+    );
 };
 
 export default App;
